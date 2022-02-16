25f07ee9c4cf8591634babcf33eb4a1d34147e38
Manvithasudhakar <85727223+Manvithasudhakar@users.noreply.github.com>
2022-02-16 12:34:28 +0530

    added the js file

--- calculator.js
+++ calculator.js
@@ -1,39 +1,22 @@
 
 function formtable()
 {    
+    document.write("<link rel='stylesheet' type='text/css' href='calculator.css'/>");
     document.write("<table border=1>");
-    document.write('<tr><th><input type="text" size="5" id="result"/></td></tr>');
-    for(let i=1;i<=9;i++)
-    {
-        if(i%3==1)
-        { 
-
-            document.write("<tr>");
+    document.write('<tr><th colspan=2><input name="textbox" type="text" size="10" id="result"/></td></tr>');
+    document.write('<th><button class="button" value="C" onclick="clr()">C</button></th></tr>');
+    document.write('<td><button class="button" onclick=cal()>=</button></td>');
+    var arr=[1,2,3,'/',4,5,6,'*',7,8,9,'+','.',0,'-'];
+    for(let i=1;i<=arr.length;i++) {
+        if(i%4==1) {
+            document.write('<tr>');
         }
-        document.write("<td>",i,"</td>");
-        if(i%3==0)
-        {
-            if(i==3)
-            {
-                document.write("<td><button  onclick='show(/)'>/</button></td>");
-            }
-            if(i==6)
-            {
-                document.write("<td><button  onclick='show(*)'>*</button></td>");
-            }
-            if(i==9) {
-                document.write("<td><button  onclick='show(+)'>+</button></td>");
-                document.write("<tr><td><button onclick='show(value)'>-</button></td>")
-                document.write("<td><button  onclick='show(.)'>.</button></td>");
-                document.write("<td><button  onclick='show(0)'>0</button></td>");
-                document.write("<td><button  onclick='cal()'>=</button></td></tr>");
-                
-            }
-
+          document.write('<td><button class="button" value=' , arr[i-1] , ' onclick=show(this.value)>',arr[i-1],'</button></td>');    
+          if(i%4==0) {
+            document.write('</tr>');
         }
-    
     }
-}
+} 
 
 function clr() {
      	document.getElementById("result").value="";
