784e1af2d79cd6e6e37d1c04d95187687e7dc830
Ananya <ananyapalimar01@gmail.com>
2022-02-16 08:24:07 +0530

    Made changes

--- calculatorjavascript.js
+++ calculatorjavascript.js
@@ -1,34 +1,21 @@
 function addTable() {
-     document.write("<link rel='stylesheet' typee='text/css' href='calculator.css' />");
-     document.write("<table border=1 id=table>")
-     document.write('<tr><th colspan=3><input name="input" size="26" id="text"></th>')
-     document.write("<td><button class='button' onclick='clearScreen()'>c</button></td></th>")
-
-	     for(let i=1; i<=9;i++) {
-		           if(i%3==1) {
-		             	document.write("<tr>");
-		                      }    
-		           document.write('<td><button class="button" value="' , i , '" onclick=display(this.value)>',i,'</td>');
-		           if(i%3==0) {
-		 	        if(i==3)  {
-		 		   document.write("<td><button class='button' value=/ onclick='display(this.value)'>/</button></td>")
-                              }
-		 	       if(i==6) {
-		 		   document.write("<td><button class='button' value=* onclick='display(this.value)'>*</button></td>")
-		 	                 }
-		 	       if(i==9)
-		 	{
-		 		document.write("<td><button class='button' value=+ onclick='display(this.value)' id='btn'>+</button></td></tr>");
-		 		document.write("<tr><td><button class='button' value=. onclick='display(this.value)'>.</button></td>");
-		 		document.write("<td><button class='button'  onclick='display(0)'>0</button></td>")
-		 		document.write("<td><button class='button' value=- onclick='display(this.value)'>-</button></td>")
-		 		document.write("<td><button class='button' onclick='cal()'>=</button></td></tr>");
-		 	}
-
-		 }   
-   }
-  }
-  function clearScreen() {
+	document.write("<link rel='stylesheet' type='text/css' href='calculator.css' />");
+    document.write('<table id="table">');
+    document.write('<tr><th colspan=3><input name="input" size="26" id="text"></th>');
+    document.write('<th><button class="button" value="C" onclick="clearScreen()">C</button></th></tr>');
+     document.write('<td><button class="button" onclick=cal()>=</button></td>');
+    var arr=[1,2,3,'/',4,5,6,'*',7,8,9,'+','.',0,'-'];
+    for(let i=1;i<=arr.length;i++) {
+        if(i%4==1) {
+            document.write('<tr>');
+        }
+          document.write('<td><button class="button" value=' , arr[i-1] , ' onclick=display(this.value)>',arr[i-1],'</button></td>');    
+          if(i%4==0) {
+            document.write('</tr>');
+        }
+    }
+} 
+ function clearScreen() {
  document.getElementById("text").value = "";
 }
 
