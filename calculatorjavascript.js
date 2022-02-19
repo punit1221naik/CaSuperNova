cb866d69532d3f04fcc337888cc8ce94af19dead
ananya <ananyapalimar01@gmail.com>
2022-02-19 08:59:06 +0530

    changes in space

--- calculatorjavascript.js
+++ calculatorjavascript.js
@@ -1,12 +1,12 @@
 function addTable() {
 	document.write("<link rel='stylesheet' type='text/css' href='calculator.css' />");
-    document.write('<table id="table">');
-    document.write('<tr><th colspan=3><input name="input" size="26" id="text"></th>');
-    document.write('<th><button class="button" value="C" onclick="clearScreen()">C</button></th></tr>');
-     document.write('<td><button class="button" onclick=cal()>=</button></td>');
-    var arr=[1,2,3,'/',4,5,6,'*',7,8,9,'+','.',0,'-'];
-    for(let i=1;i<=arr.length;i++) {
-        if(i%4==1) {
+       document.write('<table id="table">');
+       document.write('<tr><th colspan=3><input name="input" size="26" id="text"></th>');
+       document.write('<th><button class="button" value="C" onclick="clearScreen()">C</button></th></tr>');
+       document.write('<td><button class="button" onclick=cal()>=</button></td>');
+       var arr=[1,2,3,'/',4,5,6,'*',7,8,9,'+','.',0,'-'];
+       for(let i=1;i<=arr.length;i++) {
+         if(i%4==1) {
             document.write('<tr>');
         }
           document.write('<td><button class="button" value=' , arr[i-1] , ' onclick=display(this.value)>',arr[i-1],'</button></td>');    
